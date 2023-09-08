c1055b806f56acda25f5850bb4a428bae7499cbe
VadimBaby <vadimdim22@gmail.com>
2023-09-08 15:05:49 +0700

    Created the project

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Вадим Мартыненко on 08.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
